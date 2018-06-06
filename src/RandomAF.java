a7ebb1639d0eecddeb78fce0bd9ab3582f1b7713
Ian Bryan <incomplete@users.noreply.github.com>
2018-06-06 14:38:55 -0700

    RandomAF Class Added

---
+++ src/RandomAF.java
@@ -0,0 +1,21 @@
+public class RandomAF {
+    public static void main(String args[]) {
+       
+      int[] list = {16, 12, 25, 44};
+      System.out.println(findMin(list));
+    }
+    
+    public static int findMin(int[] list){
+        int min = list[0];
+            for(int i=1;i<list.length;i++){
+                if(list[i] < min){
+                    return list[i];
+                }
+                
+                else{
+                    return min;
+                }
+        }
+        return min;
+    }
+}
