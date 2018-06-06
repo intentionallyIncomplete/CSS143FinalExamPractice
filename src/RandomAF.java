3f302f84cee788a7aa4f80061c54f25b54241c4f
Ian Bryan <incomplete@users.noreply.github.com>
2018-06-06 16:17:14 -0700

    Minor Commit

--- src/RandomAF.java
+++ src/RandomAF.java
@@ -1,17 +1,15 @@
 public class RandomAF {
-    public static void main(String args[]) {
-       
-      int[] list = {16, 12, 25, 44};
-      System.out.println(findMin(list));
-    }
-    
-    public static int findMin(int[] list){
-        int min = list[0];
-            for(int i=1;i<list.length;i++){
-                if(list[i] < min){
-                    min = list[i];
-                }
-        }
-        return min;
-    }
+	public static void main(String args[]) {
+
+		int[] list = {16, 12, 25, 44};
+		System.out.println(rotateRight(list));
+	}
+
+	public static int[] rotateRight(int[] list){
+	    
+	    for(int i=list.length-1;i>0;i--){
+	        list[0] = list[i];
+	    }
+	    return list;
+	}
 }
