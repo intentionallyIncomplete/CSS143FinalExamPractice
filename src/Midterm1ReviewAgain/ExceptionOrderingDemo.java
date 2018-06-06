207a1ae6e1f6e5839725c58148e25d6e2c2bc10b
Ian Bryan <incomplete@users.noreply.github.com>
2018-06-05 17:50:05 -0700

    Exceptions Handled and Reviewed

--- src/Midterm1ReviewAgain/ExceptionOrderingDemo.java
+++ src/Midterm1ReviewAgain/ExceptionOrderingDemo.java
@@ -1,10 +1,18 @@
 package Midterm1ReviewAgain;
 
-public class ExceptionOrderingDemo {
+public class ExceptionOrderingDemo{
+	public static void main(String args[]){
+		try{
+			int a[]=new int[5];
+			a[5]=30/0;
+		}
+		catch(ArithmeticException e)
+		{System.out.println("task1 is completed");}
+		catch(ArrayIndexOutOfBoundsException e)
+		{System.out.println("task 2 completed");}
+		catch(Exception e)
+		{System.out.println("common task completed");}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("rest of the code...");
 	}
-
 }
