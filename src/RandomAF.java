d216fe83ad55877877c2e09a2dd2fa7ded09f2cb
Ian Bryan <incomplete@users.noreply.github.com>
2018-06-06 18:19:17 -0700

    More Commits

--- src/RandomAF.java
+++ src/RandomAF.java
@@ -3,40 +3,35 @@ import java.util.ArrayList;
 public class RandomAF {
 	public static void main(String args[]) {
 
-		ArrayList<String> list = new ArrayList<>();
-//		list.add("one");
-//		list.add("fourfivesix");
-//		list.add("two");
-//		list.add("three");
-		System.out.println(maxLength(list));
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(14);
+		list.add(29);
+		list.add(12);
+		list.add(7);
+		list.add(25);
+		list.add(18);
+		System.out.println(range(list));
 
 	}
 
-	public static int maxLength(ArrayList<String> list){
-
-		//ArrayList<Integer> strLengths = new ArrayList<>();
-		int maxLengthStr = 0;
-		if(list.size() == 1){
-			maxLengthStr = list.get(0).length();
-		}
-		for(int i=0;i<list.size()-1;i++){
-			if(list.get(i).length() > maxLengthStr){
-				maxLengthStr = list.get(i).length();
+	public static int range(ArrayList<Integer> list){
+	    int smallest = list.get(0); 
+	    int largest = 0;
+	    
+	    if(list.size() == 1){
+	        return 0;
+	    }
+	    for(int i=0;i<list.size();i++){
+			if(list.get(i).intValue() > largest){
+				largest = list.get(i).intValue();
 			}
 		}
-		
-		
-//		for(String s : list){
-//			strLengths.add(s.length());
-//		}
-//		
-//		for(int i : strLengths){
-//			if(i > strLengths.indexOf(i)){
-//				maxLengthStr = i;
-//			}else{
-//				maxLengthStr = strLengths.indexOf(i);
-//			}
-//		}
-		return maxLengthStr;
+	    for(int i=0;i<list.size();i++){
+			if(list.get(i).intValue() < smallest){
+				smallest = list.get(i).intValue();
+			}
+		}
+	    int range = largest-smallest;
+	    return range+1;
 	}
 }
